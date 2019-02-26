60b56b5be214e08c9672ed3510e44876d16d77fa
evaldez89 <evaldez89@gmail.com>
2019-02-26 17:57:14 -0400

    Create method for the template

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -4,13 +4,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_data(doc_path: str, x_inverse_index: int, y_index: int):
+    dataset = pd.read_csv('Data.csv')
+    x = dataset.iloc[:, :x_inverse_index].values
+    y = dataset.iloc[:, y_index].values
+    
+    return dataset, x, y
+
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-
+dataset, X, y = get_data('Salary_Data.csv', -1, 3)
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
