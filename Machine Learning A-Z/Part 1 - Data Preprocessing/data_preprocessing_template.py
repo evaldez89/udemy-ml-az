7a584266618ca92338732503dda25b2a1f20b0d6
Emmanuel Valdez <evaldez89@gmail.com>
2019-03-02 14:51:16 -0400

    Start with the simpler lineal regression analisys

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,22 +1,24 @@
 # Data Preprocessing Template
 
-# Importing the libraries
+#%% Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def get_data(doc_path: str, x_inverse_index: int, y_index: int):
-    dataset = pd.read_csv('Data.csv')
-    x = dataset.iloc[:, :x_inverse_index].values
+def get_data(dataset, x_index: int, y_index: int):
+    x = dataset.iloc[:, :x_index].values
     y = dataset.iloc[:, y_index].values
     
-    return dataset, x, y
+    return x, y
 
+#%% Importing the dataset
+doc_path = 'Salary_Data.csv'
+dataset = pd.read_csv(doc_path)
 
-# Importing the dataset
-dataset, X, y = get_data('Salary_Data.csv', -1, 3)
+#%% Get X and y
+X, y = get_data(dataset, -1, 1)
 
-# Splitting the dataset into the Training set and Test set
+#%% Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
