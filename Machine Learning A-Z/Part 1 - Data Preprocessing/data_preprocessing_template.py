a1ba4d06a1dad85d8a78be180678c440c9a73a8d
Emmanuel Valdez <evaldez89@gmail.com>
2019-03-02 15:06:05 -0400

    Fit train set to the regressor

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
 def get_data(dataset, x_index: int, y_index: int):
@@ -21,4 +22,11 @@ dataset = pd.read_csv(doc_path)
 X, y = get_data(dataset, -1, 1)
 
 #%% Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state=0)
+
+
+#%% Fitting simple linear regression in the training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#%% Predicting the test set result
