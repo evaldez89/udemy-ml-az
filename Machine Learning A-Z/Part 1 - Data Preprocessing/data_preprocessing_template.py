f10b8a8a3a80c7d1a6ed8ccc3f5081a0a77f865f
Emmanuel Valdez <evaldez89@gmail.com>
2019-03-02 20:55:40 -0400

    Multiple linear regression ready

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -26,19 +26,6 @@ def encode_categorical_data(data_set, col_index: int):
     onehotencoder = OneHotEncoder(categorical_features=[col_index])
     return onehotencoder.fit_transform(data_set).toarray()
 
-#%% Importing the dataset
-doc_path = '50_Startups.csv'
-dataset = pd.read_csv(doc_path)
-
-#%% Get X and y
-X, y = get_data(dataset, -1, 4)
-
-#%% Encode
-X = encode_categorical_data(X, 3)
-
-#%% Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state=0)
-
 # method for simple linear regression
 def simple_linear_regressor(x_set, y_set):
     # Fitting simple linear regression in the training set
@@ -55,7 +42,22 @@ def simple_linear_regressor(x_set, y_set):
     plt.ylabel('Salary ')
     plt.show()
 
-# Show plot
+#%% Importing the dataset
+doc_path = '50_Startups.csv'
+dataset = pd.read_csv(doc_path)
+
+#%% Get X and y
+X, y = get_data(dataset, -1, 4)
+
+#%% Encode
+X = encode_categorical_data(X, 3)
+
+# Avoiding the Dummy Variable Trap
+# X = X[:, 1:] The library already does it
+
+#%% Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+# Show plot for simple linear regression
 # simple_linear_regressor(X_train, y_train)
 # simple_linear_regressor(X_test, y_test)
-pd.set_option('display.float_format','{:.0f}'.format)
