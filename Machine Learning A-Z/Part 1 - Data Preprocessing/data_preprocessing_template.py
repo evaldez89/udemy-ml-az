501c505940934fe6d135f6e614033e1dea5e45c1
Emmanuel Valdez <evaldez89@gmail.com>
2019-03-03 21:37:43 -0400

    Automatic Backward elimination just P-values and P-values and Adjusted R squared

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -77,27 +77,43 @@ X = np.append(np.ones((50, 1)).astype(int), X, 1)
 
 #%% Backward Elimination - Step 2, fit the full model with all possible predictors
 # X_opt would be the optimal matrix
-# SL (significan level) = 5% (0.05)
+def backwardElimination(x, sl):  # P-values only
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+
+def backwardEliminationAdjusted(x, SL):  # P-values and Adjusted R Squared
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS = sm.OLS(y, X_opt).fit()
-regressor_OLS.summary()  # Highest is x2
-
-#%% Backward Elimination - Step 3 (remove highest)
-X_opt = X[:, [0, 1, 3, 4, 5]]
-regressor_OLS = sm.OLS(y, X_opt).fit()
-regressor_OLS.summary()  # Highest is x1
-
-#%% Backward Elimination - Step 3 ...
-X_opt = X[:, [0, 3, 4, 5]]
-regressor_OLS = sm.OLS(y, X_opt).fit()
-regressor_OLS.summary()  # Highest is x2
-
-#%% Backward Elimination - Step 3 ... more
-X_opt = X[:, [0, 3, 5]]
-regressor_OLS = sm.OLS(y, X_opt).fit()
-regressor_OLS.summary()  # Highest is x2
-
-#%% Backward Elimination - Step 3 ... more
-X_opt = X[:, [0, 3]]
-regressor_OLS = sm.OLS(y, X_opt).fit()
-regressor_OLS.summary()  # FIN
+# X_Modeled = backwardElimination(X_opt, SL)
+X_Modeled = backwardEliminationAdjusted(X_opt, SL)
